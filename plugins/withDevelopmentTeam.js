b4da20bbd9c1ef033863f4e050ebec7c85fba0c9
Xaendu123 <alex_bugnon@bluewin.ch>
2026-01-01 16:47:04 +0100

    deploy fix

--- plugins/withDevelopmentTeam.js
+++ plugins/withDevelopmentTeam.js
@@ -5,22 +5,24 @@
  * Also disables Mac and Vision Pro support to restrict to iPhone/iPad only.
  */
 
-const { withXcodeProject, withInfoPlist } = require('@expo/config-plugins');
+const { withXcodeProject, withInfoPlist, withPlugins } = require('@expo/config-plugins');
 
-module.exports = function withDevelopmentTeam(config) {
-  // First, ensure Info.plist has the display name
-  config = withInfoPlist(config, (config) => {
+function withInfoPlistDisplayName(config) {
+  return withInfoPlist(config, (config) => {
     const displayName = config.ios?.infoPlist?.CFBundleDisplayName || 
                        config.ios?.infoPlist?.CFBundleName || 
                        config.name || 
                        'Respondr';
     
-    config.modResults.CFBundleDisplayName = displayName;
+    if (!config.modResults.CFBundleDisplayName) {
+      config.modResults.CFBundleDisplayName = displayName;
+    }
     return config;
   });
+}
 
-  // Then, set build settings in Xcode project
-  config = withXcodeProject(config, async (config) => {
+function withXcodeBuildSettings(config) {
+  return withXcodeProject(config, (config) => {
     const xcodeProject = config.modResults;
     const developmentTeam = config.ios?.developmentTeam || 'U8Q2XKW88A';
     const buildNumber = config.ios?.buildNumber || '1';
@@ -32,9 +34,6 @@ module.exports = function withDevelopmentTeam(config) {
 
     // Get all build configurations
     const configurations = xcodeProject.pbxXCBuildConfigurationSection();
-    
-    // Find the main target's build configurations
-    const targetName = config.ios?.bundleIdentifier?.split('.').pop() || 'respondr';
     
     Object.keys(configurations).forEach((configUuid) => {
       const buildSettings = configurations[configUuid].buildSettings;
@@ -69,9 +68,6 @@ module.exports = function withDevelopmentTeam(config) {
         
         // Ensure only iPhone and iPad are supported
         buildSettings.TARGETED_DEVICE_FAMILY = '"1,2"'; // 1 = iPhone, 2 = iPad
-        
-        // SUPPORTED_PLATFORMS is already set correctly by Expo (iphoneos iphonesimulator)
-        // Don't modify it to avoid syntax errors - the other settings above are sufficient
       }
     });
 
@@ -83,7 +79,12 @@ module.exports = function withDevelopmentTeam(config) {
 
     return config;
   });
+}
 
-  return config;
+module.exports = function withDevelopmentTeam(config) {
+  return withPlugins(config, [
+    withInfoPlistDisplayName,
+    withXcodeBuildSettings,
+  ]);
 };
 
